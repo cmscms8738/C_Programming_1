6acd1102a4285b3ea4ca6f5393638a7259509124
cmscms8738 <cmscms8738@naver.com>
2022-11-03 14:27:20 +0900

    10th week assignment

---
+++ hw7.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int arr1[6] = { 1,2,3,4,5,6 };
+	int arr2[6] = { 7,8,9,10,11,12 };
+	int* ptr1 = &arr1[0];
+	int* ptr2 = &arr2[0];
+	int temp;
+
+	printf("arr1: ");
+	for (int j = 0; j < 6; j++)
+	{
+		printf("%d ", arr1[j]);
+	}
+	printf("\narr2: ");
+	for (int k = 0; k < 6; k++)
+	{
+		printf("%d ", arr2[k]);
+	}
+	printf("\n\n");
+
+	for (int i = 0; i < 6; i++)
+	{
+		temp = *ptr1;
+		*ptr1 = *ptr2;
+		*ptr2 = temp;
+		ptr1++;
+		ptr2++;
+	}
+	printf("after swap \narr1: ");
+	for (int j = 0; j < 6; j++)
+	{
+		printf("%d ", arr1[j]);
+	}
+	printf("\narr2: ");
+	for (int k = 0; k < 6; k++)
+	{
+		printf("%d ", arr2[k]);
+	}
+	printf("\n");
+	return 0;
+}
